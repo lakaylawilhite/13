a95943f1691dc2ce54c6a0709422555c7ca90374
lakaylawilhite <lakaylawilhite@yahoo.com>
2017-01-22 22:46:52 -0500

    final stuff

---
+++ friends.js
@@ -0,0 +1,18 @@
+//list of intial friends
+var friends = [
+	{
+		"name": "Dennis",
+		"photo": "http://dennisweaver.com/wp-content/uploads/2014/05/home00Dennis-Weaver.jpg",
+		"scores": [1,1,1,1,1,1,1,1,1,1]
+		//10
+	},
+	{
+		"name": "kayla",
+		"photo": "https://pbs.twimg.com/profile_images/iam_the_rex",
+		"scores": [5,5,5,5,5,5,5,5,5,5]
+		//55		
+	}
+];
+
+//send back to server
+module.exports = friends;
